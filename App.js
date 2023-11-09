6aaa444bedb4bca1c637c18fe065a04b4cfdf609
wooshanghyeon <woosh9912@naver.com>
2023-11-09 13:46:43 +0900

    test api 작성

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Login from "./screen/LoginScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Root from "./screen/Root";
+import { NativeBaseProvider } from "native-base";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Login></Login>
-    </NavigationContainer>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Root" component={Root} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
---
+++ api/api_post.js
@@ -0,0 +1,37 @@
+// example fetch
+// 자신의 컴퓨터 ip주소 넣기
+const myLocalIp = "172.21.48.168";
+
+const signTest = async (data) => {
+  const result = await fetch(`http://${myLocalIp}:8080/sign_up`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    // body: JSON.stringify(data)
+    body: JSON.stringify({
+      hobby: "1",
+      locationCategory: "종로구",
+      disabledCategory: "지체장애",
+      username: "local",
+      email: "abc@abc.com",
+      userId: "root",
+      passwd: "qwer1234",
+      disabledValidate: 0,
+      token: "13-1"
+      //   token: "67df9c94-bc15-4852-a6d6-0800764beb0c"
+    })
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((error) => console.log("error is --->" + error));
+
+  return result;
+};
+
+const api_post = {
+  signTest
+};
+
+export default api_post;
+// delete from user where id>=1;
