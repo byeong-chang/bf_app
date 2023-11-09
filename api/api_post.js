f283f1b33c414c79b584faf89076c7d5d9eaa08e
wooshanghyeon <woosh9912@naver.com>
2023-11-09 15:39:05 +0900

    post test

--- api/api_post.js
+++ api/api_post.js
@@ -1,6 +1,6 @@
 // example fetch
 // 자신의 컴퓨터 ip주소 넣기
-const myLocalIp = "172.21.48.168";
+const myLocalIp = "172.21.80.163";
 
 const signTest = async (data) => {
   const result = await fetch(`http://${myLocalIp}:8080/sign_up`, {
@@ -8,19 +8,19 @@ const signTest = async (data) => {
     headers: {
       "content-type": "application/json"
     },
-    // body: JSON.stringify(data)
-    body: JSON.stringify({
-      hobby: "1",
-      locationCategory: "종로구",
-      disabledCategory: "지체장애",
-      username: "local",
-      email: "abc@abc.com",
-      userId: "root",
-      passwd: "qwer1234",
-      disabledValidate: 0,
-      token: "13-1"
-      //   token: "67df9c94-bc15-4852-a6d6-0800764beb0c"
-    })
+    body: JSON.stringify(data)
+    // body: JSON.stringify({
+    //   hobby: "1",
+    //   locationCategory: "종로구",
+    //   disabledCategory: "지체장애",
+    //   username: "local",
+    //   email: "abc@abc.com",
+    //   userId: "root",
+    //   passwd: "qwer1234",
+    //   disabledValidate: 0
+    //   //   token: "13-1"
+    //   //   token: "67df9c94-bc15-4852-a6d6-0800764beb0c"
+    // })
   })
     .then((res) => res.json())
     .then((res) => console.log(res))
